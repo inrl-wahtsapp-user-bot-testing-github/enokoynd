2d240718fca8f630fd107aae0d716e075ed81c6f
inrlinrlfazfaz-inrl-faz-inrl-faz-3333 <107807702+inrlinrlfazfaz-inrl-faz-inrl-faz-3333@users.noreply.github.com>
2022-08-27 13:08:07 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,24 +1,40 @@
-/* Copyright (C) 2020 Yusuf Usta.
-
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-
-WhatsAsena - Yusuf Usta
-*/
-
 const fs = require("fs");
 const path = require("path");
-const events = require("./events");
+const events = require("./lib/inrl");
 const chalk = require('chalk');
-const config = require('./config');
-const {WAConnection, MessageType, Mimetype, Presence} = require('@adiwajshing/baileys');
-const {Message, StringSession, Image, Video} = require('./whatsasena/');
-const { DataTypes } = require('sequelize');
-const { GreetingsDB, getMessage } = require("./plugins/sql/greetings");
-const got = require('got');
+const Config = require('./config');
+const { 
+          default: makeWASocket, 
+          useSingleFileAuthState,
+          DisconbnectReason, 
+          fetchLatestBaileysVersion, 
+          makeInMemoryStore, 
+          generateForwardMessageContent, 
+          prepareWAMessageMedia, 
+          generateWAMessageFromContent, 
+          generateMessageID, 
+          downloadContentFromMessage,
+          jidDecode,
+          proto 
+} = require("@adiwajshing/baileys");
+const qrcode = require("qrcode-terminal")
 
-// Sql
-const WhatsAsenaDB = config.DATABASE.define('WhatsAsena', {
+const pino = require('pino')
+const { Boom } = require('@hapi/boom')
+const {
+           Message,
+           Image,
+           Video,
+} = require('./lib/');
+const { DataTypes } = require('sequelize');
+const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) });
+const { getMessage } = require("./plugins/sql/greetings");
+const simpleGit = require('simple-git');
+const git = simpleGit();
+const axios = require('axios');
+const got = require('got');
+//const inrl =require('inrl-db');
+const BotDB = Config.DATABASE.define('Bot', {
     info: {
       type: DataTypes.STRING,
       allowNull: false
@@ -37,14 +53,12 @@ fs.readdirSync('./plugins/sql/').forEach(plugin => {
 
 const plugindb = require('./plugins/sql/plugin');
 
-// Yalnızca bir kolaylık. https://stackoverflow.com/questions/4974238/javascript-equivalent-of-pythons-format-function //
 String.prototype.format = function () {
     var i = 0, args = arguments;
     return this.replace(/{}/g, function () {
       return typeof args[i] != 'undefined' ? args[i++] : '';
-    });
+   });
 };
-
 if (!Date.now) {
     Date.now = function() { return new Date().getTime(); }
 }
@@ -60,49 +74,21 @@ Array.prototype.remove = function() {
     return this;
 };
 
-async function whatsAsena () {
-    await config.DATABASE.sync();
-    var StrSes_Db = await WhatsAsenaDB.findAll({
-        where: {
-          info: 'StringSession'
-        }
+const { state, saveState } = useSingleFileAuthState('./session.json')
+async function whatsappBot() {
+    let { version, isLatest } = await fetchLatestBaileysVersion()
+    let conn = makeWASocket({
+        logger: pino({ level: 'silent' }),
+        printQRInTerminal: false,
+        browser: ['inrl','Safari','1.0.0'],
+        auth: state,
+        version
+         
     });
-    
-    const conn = new WAConnection();
-    const Session = new StringSession();
+   
+            store.bind(conn.ev)    
 
-    conn.logger.level = config.DEBUG ? 'debug' : 'warn';
-    var nodb;
-
-    if (StrSes_Db.length < 1) {
-        nodb = true;
-        conn.loadAuthInfo(Session.deCrypt(config.SESSION)); 
-    } else {
-        conn.loadAuthInfo(Session.deCrypt(StrSes_Db[0].dataValues.value));
-    }
-
-    conn.on ('credentials-updated', async () => {
-        console.log(
-            chalk.blueBright.italic('✅ Login information updated!')
-        );
-
-        const authInfo = conn.base64EncodedAuthInfo();
-        if (StrSes_Db.length < 1) {
-            await WhatsAsenaDB.create({ info: "StringSession", value: Session.createStringSession(authInfo) });
-        } else {
-            await StrSes_Db[0].update({ value: Session.createStringSession(authInfo) });
-        }
-    })    
-
-    conn.on('connecting', async () => {
-        console.log(`${chalk.green.bold('Whats')}${chalk.blue.bold('Asena')}
-${chalk.white.bold('Version:')} ${chalk.red.bold(config.VERSION)}
-
-${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
-    });
-    
-
-    conn.on('open', async () => {
+    conn.ev.on('open', async () => {
         console.log(
             chalk.green.bold('✅ Login successful!')
         );
@@ -138,7 +124,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
         );
     });
     
-    conn.on('message-new', async msg => {
+    conn.ev.on('message-new', async msg => {
         if (msg.key && msg.key.remoteJid == 'status@broadcast') return;
 
         if (config.NO_ONLINE) {
@@ -244,15 +230,8 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
         )
     });
 
-    try {
-        await conn.connect();
-    } catch {
-        if (!nodb) {
-            console.log(chalk.red.bold('Eski sürüm stringiniz yenileniyor...'))
-            conn.loadAuthInfo(Session.deCrypt(config.SESSION)); 
-            try {
-                await conn.connect();
-            } catch {
+    {
+        console.log('error')
                 return;
             }
         }
